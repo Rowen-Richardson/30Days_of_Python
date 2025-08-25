53854190ed0d827dd5a6b7c7b0608f3154a0cf45
Rowen Richardson <129875720+Rowen-Richardson@users.noreply.github.com>
2025-08-25 22:39:13 +0200

    Day 1 of 30 Python Challenge

---
+++ helloworld.py
@@ -0,0 +1,51 @@
+"""
+#Math and symblies 
+print(3 + 4)
+print(4 -3)
+print(4 * 3)
+print(4 / 3)
+print(4 % 3)
+print(4 ** 3)
+print(4 // 3)
+
+#String
+print("""
+"Rowen"
+"Tyler"
+"South Africa"
+"I am enjoying 30 days of Python"
+""")
+
+#Checking data types 
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Rowen"))
+print(type("Tyler"))
+print(type("South Africa"))
+
+print()
+print()
+print()
+
+#Exercise - Level 3
+print(type(97))
+print(type(5.92))
+print(type(5 + 27j))
+print(type("This is a string"))
+print(type(True))
+print(type([1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35]))
+print(type(("Mango", "Fruit", "Apple", "iPhone", "Android")))
+print(type({9, 8.2, 5.6, 9.78}))
+print(type({'name':'Rowen'}))
+
+"""
+#find the distance   (2,3) (10,8)
+
+x = (10-2) ** 2
+y = (8-3) ** 2
+
+csqrt = x + y
+print(csqrt)
