7651f351eb894c2ec3575afe9f6706172875a9cb
Rowen Richardson <129875720+Rowen-Richardson@users.noreply.github.com>
2025-08-26 21:46:40 +0200

    Day 2 of 30 Python Challenge

---
+++ Day_2_Variables_and_builtin_functions/v_b_f_learner_section.py
@@ -0,0 +1,37 @@
+"""# Variables in Python
+first_name = 'Rowen'
+last_name = 'Tyler'
+country = 'Finland'
+city = 'Helsinki'
+age = 25
+is_married = False
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+person_info = {
+   'firstname':'Asabeneh',
+   'lastname':'Yetayeh',
+   'country':'Finland',
+   'city':'Helsinki'
+   }
+
+# Printing the values stored in the variables
+
+
+print(first_name, last_name, country, age, is_married)
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('country len:', len(country))
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+"""
+
+first_name = input('What is your name: ')
+age = input('How old are you? ')
+
+print(first_name)
+print(age)
---
+++ Day_2_Variables_and_builtin_functions/variables.py
@@ -0,0 +1,58 @@
+"""
+first_name = input("enter First name: ")
+last_name  = input("enter last name: ")
+full_name = input("enter full name: ")
+country = input('what country do you live in: ')
+city = input('and the city: ')
+age = int(input('how old are you? '))
+year = int(input("what year is it? "))
+is_married = False
+is_light_on  = True
+first_name, last_name, country, year, is_married = first_name, last_name, country, year, is_married
+
+print("Welcome to python:" , full_name)
+print('we noticed you live in ', country)
+print('and this is your current personal info: ', first_name, last_name, country, year,"and you said under is married", is_married)
+"""
+""""
+print(type(first_name, last_name, country, age, is_married))
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('country len:', len(country))
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+"""
+
+
+
+#exercise 2
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_two * num_one
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two 
+floor_division = num_one // num_two 
+print(remainder)
+print(exp)
+print(floor_division)
+
+# Raduis of a circle is 30 meters
+area = 30 ** 2
+print(area)
+
+circum_of_cirlce = 3.14 * (30 * 2)
+print(circum_of_cirlce)
+
+radius = float(input('Please enter your radius: '))
+area_of_circle = radius ** 2
+print(area_of_circle)
+
+
+
