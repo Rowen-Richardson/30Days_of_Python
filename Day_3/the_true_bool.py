35880f2e06db6ea240055cacc55ce1add62369e3
Rowen Richardson <129875720+Rowen-Richardson@users.noreply.github.com>
2025-08-27 21:31:05 +0200

    Day 3 of 30 Python Challenge

---
+++ Day_3/the_true_bool.py
@@ -0,0 +1,24 @@
+age = int(input("Hi please give us your age: "))
+_height = float(input("how tall are you in feet? "))
+complex_num = 1 * 1j + 3 * 2j
+
+#area of a triangle 
+
+base = float(input("Whats the base triangel? "))
+height = float(input("Whats the height of the triangle? "))
+area = 0.5 * base * height 
+print(area)
+
+# perimeter of a rectangel 
+print("""
+      You have a triangle please input all three sides of the triangle
+      labeled a, b, and c """)
+print()
+
+a = float(input("Enter side a: "))
+b = float(input("Enter side b: "))
+c = float(input("Enter side c: "))
+
+perimeter = a + b + c
+
+print(f"The perimeter of the triangel is: {perimeter}")
